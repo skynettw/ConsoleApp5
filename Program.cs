412afe5c96cb2ad3e95f83a2211bafd8eec89220
Richard <skynet.tw@gmail.com>
2023-03-07 08:16:14 +0800

    second change

--- Program.cs
+++ Program.cs
@@ -13,7 +13,8 @@ namespace ConsoleApp5
             println("Hello world!");
             println("My Message!");
             Console.Write("My BMI is ");
-            Console.WriteLine(BMI(65.0, 1.78).ToString());
+            Console.WriteLine(BMI(65.0, 1.78));
+            Console.WriteLine(100 + 200);
         }
 
         static void println(string message)
