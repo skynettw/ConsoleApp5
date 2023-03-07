6f3adb19602e2a561196d6920294bf6b36ddae0f
Richard <skynet.tw@gmail.com>
2023-03-07 08:09:07 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            println("Hello world!");
+            println("My Message!");
+            Console.Write("My BMI is ");
+            Console.WriteLine(BMI(65.0, 1.78).ToString());
+        }
+
+        static void println(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static double BMI(double w, double h) { 
+            return Math.Round(w/Math.Pow(h, 2), 2);
+        }
+    }
+}
